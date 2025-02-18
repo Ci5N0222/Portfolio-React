2d7b3878ae58565ac16e7a439a6a1ae281b3b93b
Sion-Noh <vmfpsel@gmail.com>
2025-02-19 00:00:21 +0900

    Header logo, menu update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,34 @@
+import { useEffect, useState } from 'react'
 import styles from './Header.module.css'
+import logo from '../../assets/img/Logo01.png'
 
 export const Header = () => {
+
+  const [ scrolled, setScolled ] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScolled(window.scrollY > 50);
+    }
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+
+  }, []);
+
   return (
-    <header>
+    <header className={ scrolled ? styles.scrolled : "" }>
       <div className={ styles.container }>
         <div className={ styles.logo }>
-          header Logo
+          <img src={ logo } alt="" />
         </div>
         <div className={ styles.menu }>
-          <button>Introduce</button>
-          <button>Introduce</button>
-          <button>Introduce</button>
-          <button>Introduce</button>
+          <button>About Me</button>
+          <button>Skills</button>
+          <button>Experience</button>
+          <button>Project</button>
         </div>
       </div>
     </header>

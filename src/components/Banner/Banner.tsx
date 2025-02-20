aa7745cf95b2fa8ff837d05b6f68f89f4e788815
Sion-Noh <vmfpsel@gmail.com>
2025-02-21 01:03:51 +0900

    메인 화면 update

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,40 +1,64 @@
 import styles from './Banner.module.css'
-import profile from '../../assets/img/profile01.jpg'
+import profile01 from '../../assets/img/profile01.jpg'
+import profile02 from '../../assets/img/profile02.jpg'
+import { useEffect, useState } from 'react'
 
 export const Banner = () => {
 
-    const code = [
-        "<div>",
-        "　<p>성장하는 개발자 노시온입니다.</p>",
-        "　<p>Web Developer C-ON</p>",
-        "　<img src='c-on_image' alt='c-on_image' />",
-        "</div>"
-    ]
+    const fullCode: string = `<ddiv>
+　<p>성장하는 개발자 노시온입니다.</p>
+　<p>Web Developer C-ON</p>
+　<img src='github' />
+　<img src='tistory' />
+</div>`;
+    
 
-    return(
-        <div className={ styles.container }>
-            <div className={ styles.title }>
-                Wellcome C-ON's Home!! 
+    const [ displayedCode, setDisplayedCode ] = useState<string>("");
+    const [ displayedForm, setDisplayedForm ] = useState(false);
+
+    useEffect(() => {
+        let index: number = 0;
+        const intervalCode = setInterval(() => {
+            if (index < fullCode.length) {
+                setDisplayedCode((prev) => prev + (fullCode[index] || ""));
+                index++;
+            } else {
+                clearInterval(intervalCode); // 반복이 끝나면 타이핑 멈춤
+            }
+        }, 20);
+
+        const intervalForm = setInterval(() => {
+            setDisplayedForm(true)
+        }, 2400);
+
+        return () => {
+            clearInterval(intervalCode);
+            clearInterval(intervalForm);
+        }
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.codeWindow}>
+                <div className={styles.btn}>
+                    <p /><p /><p />
+                </div>
+                <div className={styles.codeForm}>
+                    <pre className={styles.typing}>{displayedCode}</pre>
+                </div>
             </div>
-            <div className={ styles.intro }>
-                <div className={ styles.sub }>
-                    <div className={ styles.ment }>
+            <div className={styles.intro}>
+                <div className={`${styles.sub} ${displayedForm ? styles.show : ""}`}>
+                    <div className={styles.ment}>
                         <p>성장하는 개발자 노시온입니다.</p>
                         <p>Web Developer C-ON</p>
                     </div>
-                    <img src={ profile } alt="" />
+                    <div className={ styles.imgBox }>
+                        <img src={profile01} />
+                        <img src={profile02} />
+                    </div>
                 </div>
             </div>
-            <div className={ styles.codeWindow }>
-                <div className={ styles.btn }>
-                    <p /><p /><p />
-                </div>
-                <div className={ styles.codeForm }>
-                    { code.map(item => <p>{item}</p>) }
-                </div>
-            </div>
-            
-           
         </div>
     )
 }
